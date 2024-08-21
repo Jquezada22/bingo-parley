ed68738792f5506db7d0af94284d095f7e37641b
Javier Quezada <jquezada22abc@gmail.com>
2024-08-21 00:01:28 -0500

    Update

--- src/componentes/Home.tsx
+++ src/componentes/Home.tsx
@@ -1,4 +1,7 @@
+// src/pages/Home.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './shared/Button';
 
 const Home: React.FC = () => {
     return (
@@ -6,14 +9,10 @@ const Home: React.FC = () => {
             <div className='bg-gray-300 py-8 px-4 shadow-xl rounded-md'>
                 <h1 className='text-2xl sm:text-2xl md:text-3xl lg:text-3xl text-center font-semibold'>Bienvenido a Bingo-Parley</h1>
                 <div className='my-5 mx-auto text-2xl'>
-<<<<<<< Updated upstream
                     <Link to={'/bingo'} target='_blank' rel='noopener noreferrer'>Ir al bingo</Link><br />
                     <Link to={'/ventas'} target='_blank' rel='noopener noreferrer'>Ir a las ventas</Link>
-=======
-                    <a href="/bingo" target="_blank" rel="noopener noreferrer">Ir al bingo</a><br />
-                    <a href="/ventas" target="_blank" rel="noopener noreferrer">Ir a las ventas</a>
->>>>>>> Stashed changes
                 </div>
+                <Button />
             </div>
         </div>
     );
